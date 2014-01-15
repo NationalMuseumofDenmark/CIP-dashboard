733edaa2088ece06a3d2f1c14ff0b9e09ab20e0e
Kræn Hansen <kh@bitblueprint.com>
2014-01-15 11:24:17 +0100

    Fixing syntax error.

--- data.json.php
+++ data.json.php
@@ -13,16 +13,15 @@ EOS;
     die($error_msg);
   }
 
-    $uuid_adequate_registration = '{dbafdb2c-2256-48e4-b882-0985a9eb507b}';
-    $uuid_license = '{f5d1dcd8-c553-4346-8d4d-672c85bb59be}';
-    $uuid_copyright_notice = '{af4b2e2c-5f6a-11d2-8f20-0000c0e166dc}';
+  $uuid_adequate_registration = '{dbafdb2c-2256-48e4-b882-0985a9eb507b}';
+  uuid_license = '{f5d1dcd8-c553-4346-8d4d-672c85bb59be}';
+  uuid_copyright_notice = '{af4b2e2c-5f6a-11d2-8f20-0000c0e166dc}';
 
-    $db = $m->natmus;
-    $collection = $db->cip_stats;
+  $db = $m->natmus;
+  $collection = $db->cip_stats;
 
-    $cursor = $collection->find()->sort(array('_id' => -1))->limit(1);
-    $document = $cursor->getNext();
+  $cursor = $collection->find()->sort(array('_id' => -1))->limit(1);
+  $document = $cursor->getNext();
 
-    echo json_encode($document);
-  }
+  echo json_encode($document);
 ?>
